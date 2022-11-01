e52ebf39dfdaa0fdef93a6ca19499e0ea381c116
Kulbicki Andrey <kubl-ml@mail.ru>
2022-11-01 11:04:49 +0300

    RequestMapping

--- java/org/andrew/springmvc/controllers/FirstController.java
+++ java/org/andrew/springmvc/controllers/FirstController.java
@@ -2,8 +2,10 @@ package org.andrew.springmvc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
