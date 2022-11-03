4673a1776a192bd445e26e3d7901dd2b13bb09ab
Kulbicki Andrey <kubl-ml@mail.ru>
2022-11-03 09:13:20 +0300

    get request -- annot: RequestParam

--- java/org/andrew/springmvc/controllers/FirstController.java
+++ java/org/andrew/springmvc/controllers/FirstController.java
@@ -3,6 +3,7 @@ package org.andrew.springmvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -11,9 +12,8 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request){
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
 
         System.out.println("Hello " + name + " " + surname);
 
@@ -21,7 +21,7 @@ public class FirstController {
     }
 
     @GetMapping("/goodbye")
-    public String goodByePage(){
+    public String goodByePage() {
         return "first/goodbye";
     }
 }
